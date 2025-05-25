80026707e01198e1ceae3f11792da51594dbc9ac
ggrewal99 <guruvindergrewal98@gmail.com>
2025-05-25 14:09:32 +1000

    Sort tasks by due date in getTasks function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,7 +2,9 @@ const Task = require('../models/Task');
 
 const getTasks = async (req, res) => {
 	try {
-		const tasks = await Task.find({ userId: req.user.id });
+		const tasks = await Task.find({ userId: req.user.id }).sort({
+			dueDateTime: 1,
+		});
 		res.status(200).json(tasks);
 	} catch (error) {
 		res.status(500).json({ message: 'Error getting tasks', error });
